fe253ac516041616bb7d7b35cc33a35e2010e6b4
Ramit110 <Ramit110@users.noreply.github.com>
2019-10-10 10:24:22 +0100

    Delete Secrets.java

--- ApriasalBotJava/src/main/java/com/discord/bot/ramit/Secrets.java
+++
@@ -1,6 +0,0 @@
-package com.discord.bot.ramit;
-
-public class Secrets {
-    public static final String BotID = "NjI4NTc4MDA2ODI2MzUyNjUw.XZNSUQ.qt5tjzmrrS-WGvlalzgRhAcDhts";
-    public static final String BotName = "Ramit110s_Private_Bot_Name_For_Evepraisal";
-}
